f6f2ca2ff40de661b4fcf94d0dfd134074855b52
Christian Bonzelet <christian.bonzelet@gmail.com>
2024-10-03 13:00:07 +0200

    feat: setup prompt template appsync schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,28 +1,20 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
+const schema = a
+  .schema({
+    promptTemplate: a.model({
+      id: a.id(),
+      instruction: a.string().required(),
+    }),
+  })
+  .authorization((allow) => [allow.authenticated()]);
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
+  name: "promptz",
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    defaultAuthorizationMode: "userPool",
   },
 });
 
