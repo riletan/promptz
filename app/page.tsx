9a362faca4531c29e14e1bf81c08fa9dd790c2c8
Christian Bonzelet <christian.bonzelet@gmail.com>
2024-10-25 18:24:58 +0200

    fix: move latest prompts below grid

--- app/page.tsx
+++ app/page.tsx
@@ -42,12 +42,6 @@ export default function App() {
       }
     >
       <SpaceBetween size="s">
-        <h2>Discover the latest prompts</h2>
-        <PromptCollection
-          limit={3}
-          promptsPerRow={[{ cards: 1 }, { minWidth: 500, cards: 3 }]}
-          showLoadMore={false}
-        />
         <Grid
           gridDefinition={[
             { colspan: { xxs: 12, xs: 12, s: 4, m: 4, xl: 4 } },
@@ -82,6 +76,12 @@ export default function App() {
           </div>
         </Grid>
       </SpaceBetween>
+      <h2>Discover the latest prompts</h2>
+      <PromptCollection
+        limit={3}
+        promptsPerRow={[{ cards: 1 }, { minWidth: 500, cards: 3 }]}
+        showLoadMore={false}
+      />
     </ContentLayout>
   );
 }
