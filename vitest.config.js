baca14ff3b831299ec22dddf20d967f82dd58855
Christian Bonzelet <christian.bonzelet@gmail.com>
2024-10-28 09:54:06 +0100

    test: exclude not relevant files for test coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      reporter: ["json", "text"],
+      exclude: [
+        "API.ts",
+        "mutations.ts",
+        "next.config.js",
+        "queries.ts",
+        "subscriptions.ts",
+        "commitlint.config.js",
+        "amplify/**",
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
     environment: "jsdom",
     setupFiles: ["./vitest.setup.js"],
     alias: {
