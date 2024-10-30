bc9f9dba5bde42208f4c8fa1991ec1f5423b1f24
Christian Bonzelet <christian.bonzelet@gmail.com>
2024-10-30 21:40:06 +0100

    feat: enable deletion protection for cognito user pools

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,15 @@ import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource.js";
 import { data } from "./data/resource.js";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+cfnUserPool.deletionProtection = "ACTIVE";
+
+const { amplifyDynamoDbTables } = backend.data.resources.cfnResources;
+for (const table of Object.values(amplifyDynamoDbTables)) {
+  table.deletionProtectionEnabled = true;
+}
