68f93f9b6b4a53cfaef243ec16d7966ec4335a46
Christian Bonzelet <christian.bonzelet@gmail.com>
2024-11-19 14:12:26 +0100

    fix: show seo text only on home

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,48 +2,11 @@
 
 "use client";
 
-import {
-  Box,
-  ColumnLayout,
-  Link,
-  SpaceBetween,
-} from "@cloudscape-design/components";
+import { Box, Link, SpaceBetween } from "@cloudscape-design/components";
 
 export default function Footer() {
   return (
     <div>
-      <div style={{ backgroundColor: "#f9f9fa" }}>
-        <Box margin={"xxl"} textAlign="center">
-          <ColumnLayout columns={3}>
-            <Box textAlign="center">
-              <h3>Why Use PROMPTZ?</h3>
-              Simplify software development with ready-to-use solutions.
-              Collaborate with the community to refine and share your ideas.
-              Learn prompt engineering best practices for every stage of the
-              SDLC.
-            </Box>
-            <Box textAlign="center">
-              <h3>About PROMPTZ</h3>
-              <p>
-                PROMPTZ is your ultimate resource for Amazon Q Developer prompt
-                engineering. Discover, create, and share high-quality prompts to
-                tackle real-world software development challenges. From
-                generating AWS architecture blueprints to automating workflows
-                and beyond, PROMPTZ empowers developers to get the most out of
-                Amazon Q Developer with a rich library of prompt templates and
-                community contributions.
-              </p>
-            </Box>
-            <Box textAlign="center">
-              <h3>Contribute to PROMPTZ</h3>
-              PROMPTZ is a community-first platform. The source code is open on
-              GitHub, and we welcome contributions from developers and
-              enthusiasts. If you want to share your ideas, join us on{" "}
-              <a href="https://github.com/cremich/promptz">Github</a>.
-            </Box>
-          </ColumnLayout>
-        </Box>
-      </div>
       <Box textAlign="center" margin={"xxl"}>
         <SpaceBetween size="m">
           <div>
