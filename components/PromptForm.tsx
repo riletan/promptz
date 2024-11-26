a00acc0d470e302503e4c274093a457014155379
Christian Bonzelet <christian.bonzelet@gmail.com>
2024-11-26 23:56:11 +0100

    fix: select new SDLC activities in prompt form (#61)

--- components/PromptForm.tsx
+++ components/PromptForm.tsx
@@ -59,7 +59,7 @@ const schema = yup
     sdlc: yup
       .string()
       .matches(
-        /^Plan|Requirements|Design|Implement|Test|Deploy|Maintain|Unknown$/,
+        /^Debugging|Deploy|Design|Documentation|Enhance|Implement|Operate|Optimize|Patch Management|Plan|Refactoring|Requirements|Security|Support|Test|Unknown$/,
       ),
     category: yup
       .string()
@@ -213,6 +213,7 @@ export default function PromptForm(props: PromptFormProps) {
                   <Select
                     {...field}
                     data-testid="select-sdlc"
+                    filteringType="auto"
                     selectedOption={
                       sdlcOptions.find((opt) => opt.value === field.value)!
                     }
